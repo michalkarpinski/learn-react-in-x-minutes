32174fea17530a74f5cad2fef06c1d371a375229
Michał <Micha>
2022-02-01 15:57:02 +0100

    add clear todos handling

--- src/App.js
+++ src/App.js
@@ -33,13 +33,18 @@ function App() {
     todoNameRef.current.value = null;
   }
 
+  function handleClearTodos(e) {
+    const newTodos = todos.filter((todo) => !todo.complete);
+    setTodos(newTodos);
+  }
+
   return (
     <>
       <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input ref={todoNameRef} type="text" />
       <button onClick={handleAddTodo}>Add Todo</button>
-      <button>Clear Completed Todos</button>
-      <div>0 left to do</div>
+      <button onClick={handleClearTodos}>Clear Completed Todos</button>
+      <div>{todos.filter((todo) => !todo.complete).length} left to do</div>
     </>
   );
 }
